79812922c5cecc2e00878ab2e0143cdeed8740c3
Diana Muñoz <91489788+DIANAIMC@users.noreply.github.com>
2022-11-28 17:13:06 -0600

    Update API_python.py

--- API_python.py
+++ API_python.py
@@ -1,10 +1,18 @@
 import requests
 
+#Credenciales de Spotify
 CLIENT_ID = 'af1707ed062448f9aa96ffd1b36737ac'
 CLIENT_SECRET = '017e9f625cc149b8b1e11c69bd21ef1d'
+#Token de autenticación para el GET
 ACCESS_TOKEN = 'BQCRhrP7ri9VTAsnLMrYn1SEuqHJvaI3KC8hsYBQEP0_jc_zTAKncfMaiqlntylT1IW9QoynbnQT8kFV_uVou0YY-ucI2_i_8T4qOOXw3iaPvFaW2g1Cun94mWXwQg7Acq-N0gTTNfyYfBoqfsWlASKfFyoLOCpTl-Tty71_VQ5cHRJMQl4UY7PFGz-mE1LXdd0'
-SPOTIFY_GET_ITEM = 'https://api.spotify.com/v1/search?q=A&type=artist&limit=2&offset=0'
 
+"""
+    Este método nos permite extraer artistas de Spotify (aquellos que contienen una A) de 1 en 1 hasta el "total" y los agrega a la lista "artist" la cual posteriormente usaremos
+    para insertar los asrtistas en Mongo.
+    Input:
+        access_token: Autenticación necesaria para el request
+        total: cantidad de artistas que queremos extraer. Nota: solo hay aproximadamente 10,000 artistas
+"""
 def get_artist1(access_token,total):
     artists = []
     offset=0
@@ -18,6 +26,7 @@ def get_artist1(access_token,total):
             }
         )
         json_resp = response.json()
+        #Solo extraemos información relevante del artista
         artist_id = json_resp["artists"]['items'][0]['id']
         artist_name = json_resp["artists"]['items'][0]['name']
         genres = json_resp["artists"]['items'][0]['genres']
