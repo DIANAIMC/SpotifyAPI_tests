c8438e60cef82bb9d00ac61f52af2c6783707263
Mariano Alcaraz Aguilar <marianoaaguilar@hotmail.com>
2022-11-29 22:44:11 -0600

    cambio límtes de búsqueda de años

--- api_mariano.py
+++ api_mariano.py
@@ -35,18 +35,18 @@ def get_data(access_token, lista, limite, year, type):
 
 print('Obteniendo artistas')
 artistas = []
-for year in range(2020,2022):
+for year in range(2000,2022):
    artistas = get_data(ACCESS_TOKEN, artistas, 1000, year, 'artist')
 print(len(artistas))
 
 print('\nObteniendo albums')
 albums = []
-for year in range(2020,2022):
+for year in range(2000,2022):
    albums = get_data(ACCESS_TOKEN, albums, 1000, year, 'album')
 print(len(albums))
 
 print('\nObteniendo tracks')
 tracks = []
-for year in range(2020,2022):
+for year in range(2000,2022):
    tracks = get_data(ACCESS_TOKEN, tracks, 1000, year, 'track')
 print(len(tracks))
