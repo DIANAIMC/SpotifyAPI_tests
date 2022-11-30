6045a59697ff38727e06123ccce95685aa9b80bb
Mariano Alcaraz Aguilar <marianoaaguilar@hotmail.com>
2022-11-29 22:56:02 -0600

    Agrego limpieza de repetidos

--- api_mariano.py
+++ api_mariano.py
@@ -1,6 +1,7 @@
 import requests
 import time
 from pymongo import MongoClient
+from iteration_utilities import unique_everseen
 
 ACCESS_TOKEN = 'BQA4xzbXSIP_yIpiZ6aXxE9VHKd0cZszTJr_mSZsy5wVkdD7Hcud8_dSnakjhISx_F8FYEitly-AVKZRtsh6P8sIzgEJrV69ylX5ONp8bZxwVIMRZ2GN78rWBvoQ0WPO0BH1rHlXcz6FOKO168O42t9__ljjIpmlIsl9yJoJvFE_9rko'
 
@@ -41,18 +42,27 @@ artistas = []
 for year in range(anio_inicio, anio_fin):
    print(f'Año {year}')
    artistas = get_data(ACCESS_TOKEN, artistas, 1000, year, 'artist')
-print(len(artistas))
+# Limpiamos los artistas para que no haya repetidos
+artistas_final = list(unique_everseen(artistas))
+print(f'Previo a limpieza: {len(artistas)}')
+print(f'Posterior a limpieza: {len(artistas_final)}')
 
 print('\nObteniendo albums')
 albums = []
 for year in range(anio_inicio, anio_fin):
    print(f'Año {year}')
    albums = get_data(ACCESS_TOKEN, albums, 1000, year, 'album')
-print(len(albums))
+# Limpiamos los albums para que no haya repetidos
+albums_final = list(unique_everseen(albums))
+print(f'Previo a limpieza: {len(albums)}')
+print(f'Posterior a limpieza: {len(albums_final)}')
 
 print('\nObteniendo tracks')
 tracks = []
 for year in range(anio_inicio, anio_fin):
    print(f'Año {year}')
    tracks = get_data(ACCESS_TOKEN, tracks, 1000, year, 'track')
-print(len(tracks))
+# Limpiamos los tracks para que no haya repetidos
+tracks_final = list(unique_everseen(tracks))
+print(f'Previo a limpieza: {len(tracks)}')
+print(f'Posterior a limpieza: {len(tracks_final)}')
