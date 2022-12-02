46525fd682c5f50a9be64a196b585da9f7c22051
Mariano Alcaraz Aguilar <marianoaaguilar@hotmail.com>
2022-12-02 16:35:30 -0600

    elimino colecciones antes de insertar para que no haya repetidos

--- create_container.sh
+++ create_container.sh
@@ -6,3 +6,13 @@ docker rm spotify
 docker volume create spotify-data
 # Creamos el contenedor con el volumen creado y con mongo como imagen 
 docker run -d --name spotify -p 27017:27017 --mount source=spotify-data,target=/data mongo
+
+# Eliminamos las colecciones en caso de que existan
+docker exec -it spotify mongosh --quiet \
+--eval 'use spotify'
+--eval 'db.artists.drop()' \
+--eval 'db.albums.drop()' \
+--eval 'db.tracks.drop()' \
+--eval 'db.uw_artists.drop()' \
+--eval 'db.uw_albums.drop()' \
+--eval 'db.uw_tracks.drop()'
