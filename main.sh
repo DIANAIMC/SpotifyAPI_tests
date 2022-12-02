aabb460babfc0d65e469d0ef4f3c62ff4273065b
Mariano Alcaraz Aguilar <marianoaaguilar@hotmail.com>
2022-12-02 17:57:44 -0600

    cambio de lugar los scripts

--- main.sh
+++ main.sh
@@ -2,12 +2,12 @@
 
 # Primero creamos el contenedor
 echo '---------------------CREACIÓN DEL CONTENEDOR---------------------'
-./create_container.sh
+./scripts/create_container.sh
 
 # Luego extraemos los datos de la API
 echo -e '\n------------------------OBTENCIÓN DE DATOS------------------------'
-python ./spotify.py
+python ./scripts/spotify.py
 
 # Y ahora sacamos los datos de mongo y los traemos a nuestra computadora
 echo '-------------------EXTRACCIÓN DE DATOS DE MONGO-------------------'
-./extract_data_from_mongo.sh
+./scripts/extract_data_from_mongo.sh
---
+++ scripts/make_unwinds.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+# Hacemos el unwind de los artistas sobre los géneros
+docker exec -it spotify mongosh --quiet \
+--eval 'use spotify' \
+--eval 'db.artists.aggregate([{$unwind:"$genres"}, {$project:{_id:0}}, {$out:"uw_artists"}])'
+
+# Hacemos el unwind de albums sobre available_markets
+docker exec -it spotify mongosh --quiet \
+--eval 'use spotify' \
+--eval 'db.albums.aggregate([{$unwind:"$artists"}, {$project:{_id:0}}, {$out:"uw_albums"}])'
+
+# Hacemos el unwind de tracks sobre available_markets
+docker exec -it spotify mongosh --quiet \
+--eval 'use spotify' \
+--eval 'db.tracks.aggregate([{$unwind:"$artists"}, {$project:{_id:0}}, {$out:"uw_tracks"}])'
