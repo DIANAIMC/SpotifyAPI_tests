8154db1378102c1ad931e3c26e02f2354d9eabcd
Mariano Alcaraz Aguilar <marianoaaguilar@hotmail.com>
2022-12-02 17:04:17 -0600

    ultimos cambios

--- main.sh
+++ main.sh
@@ -5,7 +5,7 @@ echo '---------------------CREACIÓN DEL CONTENEDOR---------------------'
 ./create_container.sh
 
 # Luego extraemos los datos de la API
-echo '------------------------OBTENCIÓN DE DATOS------------------------'
+echo -e '\n------------------------OBTENCIÓN DE DATOS------------------------'
 python ./spotify.py
 
 # Y ahora sacamos los datos de mongo y los traemos a nuestra computadora
