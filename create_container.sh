b82b2489604a314e98e4557154442c474a79ebce
Mariano Alcaraz Aguilar <marianoaaguilar@hotmail.com>
2022-12-02 16:38:02 -0600

    arreglo typo y cambio el inico de los datos

--- create_container.sh
+++ create_container.sh
@@ -7,9 +7,12 @@ docker volume create spotify-data
 # Creamos el contenedor con el volumen creado y con mongo como imagen 
 docker run -d --name spotify -p 27017:27017 --mount source=spotify-data,target=/data mongo
 
+# Iniciamos el contenedor
+docker start spotify
+
 # Eliminamos las colecciones en caso de que existan
 docker exec -it spotify mongosh --quiet \
---eval 'use spotify'
+--eval 'use spotify' \
 --eval 'db.artists.drop()' \
 --eval 'db.albums.drop()' \
 --eval 'db.tracks.drop()' \
