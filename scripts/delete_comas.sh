ca58f1a7b3c29cdea61167e84a4f68c80f752567
Mariano Alcaraz Aguilar <marianoaaguilar@hotmail.com>
2022-12-03 14:53:23 -0600

    otro

--- scripts/delete_comas.sh
+++ scripts/delete_comas.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 
-echo -e 'Eliminamos posibles datos con comas no deseadas: \n'
 docker exec -it spotify mongosh \
 --quiet \
 --eval 'db.uw_artists.updateMany({name:/.*/},{ $set: { name: { $replace: [ /,/g, "" ] }}})' \
@@ -15,5 +14,3 @@ docker exec -it spotify mongosh \
 --quiet \
 --eval 'db.uw_tracks.updateMany({name:/.*/},{ $set: { name: { $replace: [ /,/g, "" ] }}})' \
 > /dev/null
-
-echo -e '\n'
