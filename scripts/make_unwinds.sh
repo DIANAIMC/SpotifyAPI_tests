47ae90cefb2bfa2fd976418d74ac9b3f9a178e0c
Mariano Alcaraz Aguilar <marianoaaguilar@hotmail.com>
2022-12-02 18:49:14 -0600

    ya ahora sí final

--- scripts/make_unwinds.sh
+++ scripts/make_unwinds.sh
@@ -3,14 +3,17 @@
 # Hacemos el unwind de los artistas sobre los géneros
 docker exec -it spotify mongosh --quiet \
 --eval 'use spotify' \
---eval 'db.artists.aggregate([{$unwind:"$genres"}, {$project:{_id:0}}, {$out:"uw_artists"}])'
+--eval 'db.artists.aggregate([{$unwind:"$genres"}, {$project:{_id:0}}, {$out:"uw_artists"}])' #\
+# --eval 'db.uw_artists.updateMany({name:/.*/},{ $set: { name: { $replace: [ /,/g, "" ] }}})'
 
 # Hacemos el unwind de albums sobre available_markets
 docker exec -it spotify mongosh --quiet \
 --eval 'use spotify' \
---eval 'db.albums.aggregate([{$unwind:"$artists"}, {$project:{_id:0}}, {$out:"uw_albums"}])'
+--eval 'db.albums.aggregate([{$unwind:"$artists"}, {$project:{_id:0}}, {$out:"uw_albums"}])' # \
+# --eval 'db.uw_albums.updateMany({name:/.*/},{ $set: { name: { $replace: [ /,/g, "" ] }}})'
 
 # Hacemos el unwind de tracks sobre available_markets
 docker exec -it spotify mongosh --quiet \
 --eval 'use spotify' \
---eval 'db.tracks.aggregate([{$unwind:"$artists"}, {$project:{_id:0}}, {$out:"uw_tracks"}])'
+--eval 'db.tracks.aggregate([{$unwind:"$artists"}, {$project: {_id:0}}, {$out:"uw_tracks"}])' # \
+# --eval 'db.uw_tracks.updateMany({name:/.*/}, { $set: {name: {$replace: [ /,/g, "" ] }}})'
--- scripts/obtencion_csv.sh
+++ scripts/obtencion_csv.sh
@@ -7,8 +7,11 @@ echo 'artist_id,album_id,name,release_date,total_tracks' > $WORKING_DIR/csv/albu
 echo 'album_id,artist_id,disc_number,duration_ms,explicit,track_id,name,popularity,track_number' > $WORKING_DIR/csv/tracks.csv 
 
 echo 'Convirtiendo artistas'
+sleep 1
 jq -r '[.followers.total, .genres, .id, .name, .popularity] | @csv' $WORKING_DIR/data_spotify/artists.json | awk -F, '{printf "%s,%s,%s,%s,%s\n", $1, $2, $3, $4, $5}' >> $WORKING_DIR/csv/artists.csv
 echo 'Convirtiendo albums'
+sleep 1
 jq -r '[.artists.id, .id, .name, .release_date, .total_tracks] | @csv' $WORKING_DIR/data_spotify/albums.json | awk -F, '{printf "%s,%s,%s, %s,%s\n", $1, $2, $3, $4, $5}' >> $WORKING_DIR/csv/albums.csv
 echo 'Convirtiendo tracks'
+sleep 1
 jq -r '[.album.id, .artists.id, .disc_number, .duration_ms, .explicit, .id, .name, .popularity, .track_number] | @csv' $WORKING_DIR/data_spotify/tracks.json | awk -F, '{printf "%s,%s,%s,%s,%s,%s,%s,%s,%s\n", $1, $2, $3, $4, $5, $6, $7, $8, $9}' >> $WORKING_DIR/csv/tracks.csv
