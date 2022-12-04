1f18d69ebc36d16e4c4df6f16e9038ea6a2cc638
Sebastián Murillo García <murillo.sebas.g@gmail.com>
2022-12-04 07:52:41 -0600

    modificacion name

--- scripts/obtencion_csv.sh
+++ scripts/obtencion_csv.sh
@@ -8,10 +8,10 @@ echo 'album_id,artist_id,disc_number,duration_ms,explicit,track_id,name,populari
 
 echo 'Convirtiendo artistas'
 sleep 1
-jq -r '[.followers.total, .genres, .id, .name, .popularity] | @csv' $WORKING_DIR/data_spotify/artists.json | awk -F, '{printf "%s,%s,%s,%s,%s\n", $1, $2, $3, $4, $5}' >> $WORKING_DIR/csv/artists.csv
+jq -r '[.followers.total, .genres, .id, .popularity, .name] | @csv' $WORKING_DIR/data_spotify/artists.json | awk -F, '{printf "%s,%s,%s,%s,%s\n", $1, $2, $3, $4, $5}' >> $WORKING_DIR/csv/artists.csv
 echo 'Convirtiendo albums'
 sleep 1
-jq -r '[.artists.id, .id, .name, .release_date, .total_tracks] | @csv' $WORKING_DIR/data_spotify/albums.json | awk -F, '{printf "%s,%s,%s, %s,%s\n", $1, $2, $3, $4, $5}' >> $WORKING_DIR/csv/albums.csv
+jq -r '[.artists.id, .id, .release_date, .total_tracks, .name] | @csv' $WORKING_DIR/data_spotify/albums.json | awk -F, '{printf "%s,%s,%s, %s,%s\n", $1, $2, $3, $4, $5}' >> $WORKING_DIR/csv/albums.csv
 echo 'Convirtiendo tracks'
 sleep 1
-jq -r '[.album.id, .artists.id, .disc_number, .duration_ms, .explicit, .id, .name, .popularity, .track_number] | @csv' $WORKING_DIR/data_spotify/tracks.json | awk -F, '{printf "%s,%s,%s,%s,%s,%s,%s,%s,%s\n", $1, $2, $3, $4, $5, $6, $7, $8, $9}' >> $WORKING_DIR/csv/tracks.csv
+jq -r '[.album.id, .artists.id, .disc_number, .duration_ms, .explicit, .id, .popularity, .track_number, .name] | @csv' $WORKING_DIR/data_spotify/tracks.json | awk -F, '{printf "%s,%s,%s,%s,%s,%s,%s,%s,%s\n", $1, $2, $3, $4, $5, $6, $7, $8, $9}' >> $WORKING_DIR/csv/tracks.csv
