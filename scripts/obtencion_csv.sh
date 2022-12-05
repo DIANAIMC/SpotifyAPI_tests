d99931a99ac925b417b66bab089a96222e2caa66
DIANAIMC <dianamunnoz@LAPTOP-DLR8O4OC>
2022-12-04 22:57:31 -0600

    sufro

--- scripts/obtencion_csv.sh
+++ scripts/obtencion_csv.sh
@@ -9,24 +9,21 @@ echo 'available_market,album_id,release_date,total_tracks,namee' > $WORKING_DIR/
 echo 'album_id,available_market,disc_number,duration_ms,explicit,track_id,popularity,track_number,namee' > $WORKING_DIR/csv/monet/tracks.csv 
 
 echo -e '\nConvirtiendo artistas'
-sleep 1
 jq -r '[.followers.total, .genres, .id, .popularity, .name] | @csv' $WORKING_DIR/data_spotify/artists_mon.json | awk -F, '{printf "%s,%s,%s,%s,%s\n", $1, $2, $3, $4, $5}' >> $WORKING_DIR/csv/monet/artists.csv
 echo 'Convirtiendo albums'
-sleep 1
 jq -r '[.available_markets, .id, .release_date, .total_tracks, .name] | @csv' $WORKING_DIR/data_spotify/albums_mon.json | awk -F, '{printf "%s,%s,%s, %s,%s\n", $1, $2, $3, $4, $5}' >> $WORKING_DIR/csv/monet/albums.csv
 echo 'Convirtiendo tracks'
-sleep 1
 jq -r '[.album.id, .available_markets, .disc_number, .duration_ms, .explicit, .id, .popularity, .track_number, .name] | @csv' $WORKING_DIR/data_spotify/tracks_mon.json | awk -F, '{printf "%s,%s,%s,%s,%s,%s,%s,%s,%s\n", $1, $2, $3, $4, $5, $6, $7, $8, $9}' >> $WORKING_DIR/csv/monet/tracks.csv
  
 #Ahora vamos a agregar las comillas de cierre que falten a los nombres de artista, album y track.
 #(Esto pasa porque hay artistas, albumes y tracks que tienen comas en su nombre)
 
 #Artists
-cat $WORKING_DIR/csv/monet/artists.csv | sed 's/.$//' | sed 's/$/"/' | sed '1s/.$//' | sed "$ d" > $WORKING_DIR/csv/monet/artists_mon.csv
+cat $WORKING_DIR/csv/monet/artists.csv | sed 's/.$//' | sed 's/$/"/' | sed '1s/.$//' | sed "$ d" | sed 's/"//g' > $WORKING_DIR/csv/monet/artists_mon.csv
 #Albums
-cat $WORKING_DIR/csv/monet/albums.csv | sed 's/.$//' | sed 's/$/"/' | sed '1s/.$//' | sed "$ d" > $WORKING_DIR/csv/monet/albums_mon.csv
+cat $WORKING_DIR/csv/monet/albums.csv | sed 's/.$//' | sed 's/$/"/' | sed '1s/.$//' | sed "$ d" | sed 's/"//g' > $WORKING_DIR/csv/monet/albums_mon.csv
 #Tracks
-cat $WORKING_DIR/csv/monet/tracks.csv | sed 's/.$//' | sed 's/$/"/' | sed '1s/.$//' | sed "$ d" > $WORKING_DIR/csv/monet/tracks_mon.csv
+cat $WORKING_DIR/csv/monet/tracks.csv | sed 's/.$//' | sed 's/$/"/' | sed '1s/.$//' | sed "$ d" | sed 's/"//g' > $WORKING_DIR/csv/monet/tracks_mon.csv
 
 #Eliminamos los csv auxiliares
 rm $WORKING_DIR/csv/monet/artists.csv
@@ -40,13 +37,10 @@ echo 'artist_id,album_id,release_date,total_tracks,namee' > $WORKING_DIR/csv/neo
 echo 'album_id,artist_id,disc_number,duration_ms,explicit,track_id,popularity,track_number,namee' > $WORKING_DIR/csv/neo4j/tracks.csv 
 
 echo -e '\nConvirtiendo artistas'
-sleep 1
 jq -r '[.followers.total, .genres, .id, .popularity, .name] | @csv' $WORKING_DIR/data_spotify/artists_neo.json | awk -F, '{printf "%s,%s,%s,%s,%s\n", $1, $2, $3, $4, $5}' >> $WORKING_DIR/csv/neo4j/artists.csv
 echo 'Convirtiendo albums'
-sleep 1
 jq -r '[.artists.id, .id, .release_date, .total_tracks, .name] | @csv' $WORKING_DIR/data_spotify/albums_neo.json | awk -F, '{printf "%s,%s,%s, %s,%s\n", $1, $2, $3, $4, $5}' >> $WORKING_DIR/csv/neo4j/albums.csv
 echo 'Convirtiendo tracks'
-sleep 1
 jq -r '[.album.id, .artists.id, .disc_number, .duration_ms, .explicit, .id, .popularity, .track_number, .name] | @csv' $WORKING_DIR/data_spotify/tracks_neo.json | awk -F, '{printf "%s,%s,%s,%s,%s,%s,%s,%s,%s\n", $1, $2, $3, $4, $5, $6, $7, $8, $9}' >> $WORKING_DIR/csv/neo4j/tracks.csv
  
 #Ahora vamos a agregar las comillas de cierre que falten a los nombres de artista, album y track.
