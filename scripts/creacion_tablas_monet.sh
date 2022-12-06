26f1ffb49bc57556e3ae754931e728baa5d221e8
Mariano Alcaraz Aguilar <marianoaaguilar@hotmail.com>
2022-12-05 21:37:23 -0600

    cambio nombre columna album_id

--- scripts/creacion_tablas_monet.sh
+++ scripts/creacion_tablas_monet.sh
@@ -6,7 +6,7 @@ echo "- Tabla artist"
 docker exec -i monetdb mclient -d spotify -s "create table if not exists artist(followers varchar(100), genre varchar(100),artist_id varchar(100),popularity varchar(100),name varchar(200));"
 
 echo "- Tabla album"
-docker exec -i monetdb mclient -d spotify -s "create table if not exists album(available_market varchar(100),artist_id varchar(100),release_date varchar(100),total_tracks varchar(100),name varchar(200));"
+docker exec -i monetdb mclient -d spotify -s "create table if not exists album(available_market varchar(100),album_id varchar(100),release_date varchar(100),total_tracks varchar(100),name varchar(200));"
 
 echo "- Tabla track"
 docker exec -i monetdb mclient -d spotify -s "create table if not exists track(album_id varchar(100),available_market varchar(100),disc_number varchar(100),duration_ms varchar(100),explicit varchar(100), track_id varchar(50),popularity varchar(50),track_number varchar(50),name varchar(200));"
