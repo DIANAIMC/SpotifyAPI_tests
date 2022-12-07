0143d9a05976384a918345843aecd2e87243f355
Sebastián Murillo García <murillo.sebas.g@gmail.com>
2022-12-06 20:41:46 -0600

    last query

--- queries/monetdb.sql
+++ queries/monetdb.sql
@@ -12,3 +12,13 @@ FROM album a
 GROUP BY a.available_market
 ORDER BY num_albums ASC, total_tracks ASC
 LIMIT 10;
+
+-- Query para encontrar los TOP 50 albumes del año de la pandemia 
+-- basándonos en la popularidad de sus tracks.
+SELECT a.name, AVG(t.popularity) as avg_popularity_tracks
+FROM album a
+JOIN track t ON a.album_id = t.album_id
+WHERE SUBSTR(a.release_date, 1, 4) BETWEEN 2020 AND 2021
+GROUP BY a.name
+ORDER BY avg_popularity_tracks DESC
+LIMIT 50;
