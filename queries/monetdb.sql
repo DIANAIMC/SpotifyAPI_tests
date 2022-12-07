49ca7848fff40545d43c6dabc6e361c97eddd061
Diana Muñoz <91489788+DIANAIMC@users.noreply.github.com>
2022-12-06 20:33:34 -0600

    Update monetdb.sql

--- queries/monetdb.sql
+++ queries/monetdb.sql
@@ -4,3 +4,11 @@ FROM album a
 JOIN track t ON a.available_market = t.available_market AND a.album_id = t.album_id
 GROUP BY a.available_market, explicit
 ORDER BY a.available_market, explicit ASC;
+
+-- Muestra los mercados con menor número de albums y a su vez muestra el promedio de canciones por mercado
+SELECT a.available_market, COUNT(*) as num_albums,
+AVG(a.total_tracks) AS total_tracks
+FROM album a
+GROUP BY a.available_market
+ORDER BY num_albums ASC, total_tracks ASC
+LIMIT 10;
